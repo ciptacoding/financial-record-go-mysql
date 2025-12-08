28862fabff232b13ba2f6f9ab263e0899796624c
ciptacoding <ciptacoding@gmail.com>
2025-12-08 14:57:29 +0700

    fix: update charset to utf8mb4_unicode_ci in financial_record.sql

--- financial_record.sql
+++ financial_record.sql
@@ -54,7 +54,7 @@ CREATE TABLE `users` (
   `photo` text,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Indexes for dumped tables
